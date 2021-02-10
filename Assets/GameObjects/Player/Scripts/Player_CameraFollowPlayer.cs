3dd6fe408e6c3ed7238d901e02c2555ed9683e0b
Johnny Howe <jonathon.o.howe@gmail.com>
2021-02-10 22:46:31 +1300

    feat: player side to side movement w/ camera

---
+++ Assets/GameObjects/Player/Scripts/Player_CameraFollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_CameraFollowPlayer : MonoBehaviour
+{
+    public Vector3 positionOffset = new Vector3(0, 5, -5);
+    public Vector3 eulerAngleOffset = new Vector3(30, 0, 0);
+    public float angleMultiplier = Mathf.PI;
+
+    void Update()
+    {
+        Camera.main.transform.position = transform.position + positionOffset;    
+        Camera.main.transform.eulerAngles = eulerAngleOffset + Vector3.forward * transform.position.x * angleMultiplier;
+    }
+}
---
+++ Assets/GameObjects/Player/Scripts/Player_Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // number between -1 and 1
+    public float horizontalPosition = 0;
+    public float maxMovement = 0.8f;
+    public float movementMultiplier = 1;
+    public float rotationSpeed = 1f;
+    public float movementSpeed = 5;
+    public float radius = 1;
+    Vector2 rotateOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateOrigin = (Vector2) transform.position + Vector2.up * radius;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (TouchInput.touched)
+        {
+            horizontalPosition = Mathf.Min(Mathf.Max(-maxMovement, TouchInput.screenPosition.x * 2 - 1), maxMovement);
+
+            float lastX = transform.position.x;
+            float nextX = horizontalPosition * movementMultiplier;
+            float smoothX = transform.position.x - (lastX - nextX) * movementSpeed * Time.deltaTime;
+
+            transform.position = GetPositionOnCircle(smoothX, radius, rotateOrigin);
+        }
+    }
+
+    Vector2 GetPositionOnCircle(float x, float radius, Vector2 origin)
+    {
+        float ox = x - origin.x;
+        float y;
+        if (ox >= radius)
+        {
+            y = origin.y;
+            x = origin.x + radius;
+        }
+        else if (ox <= -radius)
+        {
+            y = origin.y;
+            x = origin.x - radius;
+        } else
+        {
+            y = -Mathf.Sqrt(radius * radius - ox * ox) + origin.y;
+        }
+
+        return new Vector2(x, y);
+    }
+}
